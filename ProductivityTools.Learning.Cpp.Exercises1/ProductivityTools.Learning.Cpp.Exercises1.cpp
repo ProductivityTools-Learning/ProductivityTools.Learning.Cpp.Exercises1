f3c6497a8158aa46741d48ccf76dc64962174117
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-05-29 23:30:26 +0200

    tupes

--- ProductivityTools.Learning.Cpp.Exercises1/ProductivityTools.Learning.Cpp.Exercises1.cpp
+++ ProductivityTools.Learning.Cpp.Exercises1/ProductivityTools.Learning.Cpp.Exercises1.cpp
@@ -7,6 +7,8 @@
 using std::cout;
 
 void printName(std::string);
+void sizes();
+void chars();
 
 int main()
 {
@@ -26,10 +28,32 @@ int main()
 		++r;
 	};
 
-
+	sizes();
+	chars();
 }
 
 void printName(std::string name)
 {
 	cout << "HEllo" << name << std::endl;
+}
+
+void sizes()
+{
+	int n_int = INT32_MAX;
+	short n_short = SHRT_MAX;
+	cout << "Int is a size of" << sizeof(n_int) << std::endl;
+	cout << "Short is a size of" << sizeof(n_short) << std::endl;
+	cout << "size of the variable" << sizeof n_int << std::endl;
+ }
+
+void chars()
+{
+	char c = 'P';
+	c = c + 1;
+	cout << "Interesting char is" << c << std::endl;
+
+	int i = 40;
+	cout << "char from the number. Number: " << i << " the representing char";
+	cout.put(i);
+	cout << std::endl;
 }
